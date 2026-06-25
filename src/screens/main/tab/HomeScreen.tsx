14aa8c342772215ba9a620f6f87f6be644a77f85
naresh-sukhwal <nsukhwal1527.com>
2026-06-25 13:07:25 +0530

    Added new features

--- src/screens/main/tab/HomeScreen.tsx
+++ src/screens/main/tab/HomeScreen.tsx
@@ -1,60 +1,12 @@
-import { StyleSheet, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import { useThemedStyles } from '@/hooks/useThemedStyles';
-import * as types from '@/interface';
-import MainHeader from '@/components/headers/MainHeader';
-import { useTranslation } from 'react-i18next';
-import { wp, hp } from '@/utils/responsive.utils';
-import { getSalutation } from '@/utils/helper.utils';
-
-import HomeGoldCard from '@/components/layouts/home/HomeGoldCard';
-import HomeStatsRow from '@/components/layouts/home/HomeStatsRow';
-import HomeQuickActions from '@/components/layouts/home/HomeQuickActions';
-import HomeGoldTrend from '@/components/layouts/home/HomeGoldTrend';
-import HomeRecentActivity from '@/components/layouts/home/HomeRecentActivity';
-import InfoComponent from '@/components/layouts/common/InfoComponent';
 
 export default function HomeScreen() {
-  const styles = useThemedStyles(createStyles);
-  const { t } = useTranslation();
-
   return (
-    <View style={styles.container}>
-      <MainHeader greeting={t(getSalutation())} name="Aarav" />
-
-      <ScrollView
-        contentContainerStyle={styles.scrollContent}
-        showsVerticalScrollIndicator={false}
-      >
-        <HomeGoldCard />
-        <HomeStatsRow />
-        <HomeQuickActions />
-        <HomeGoldTrend />
-        <HomeRecentActivity />
-
-        <InfoComponent
-          isDark
-          title={t('home.secure_withdrawals')}
-          description={t('home.secure_withdrawals_desc')}
-          customIcon="shield-checkmark-outline"
-          style={styles.footerInfo}
-        />
-      </ScrollView>
+    <View>
+      <Text>HomeScreen</Text>
     </View>
   );
 }
 
-const createStyles = (themeColor: types.themeType) =>
-  StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: themeColor.backgroundColor,
-    },
-    scrollContent: {
-      paddingHorizontal: wp('5%'),
-      paddingBottom: hp('15%'),
-    },
-    footerInfo: {
-      marginTop: 32,
-    },
-  });
+const styles = StyleSheet.create({});
